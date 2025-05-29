83d5a3c2c292de6a8e874346dcf565c42b3556ce
DawidK4 <s27637@pjwstk.edu.pl>
2025-05-29 20:06:24 +0200

    added binary_search

--- search.py
+++ search.py
@@ -19,3 +19,30 @@ def k_jump(arr, key, k):
             return j
     return -1
 
+def binary_search(arr, key):
+    """
+    Implements the binary search algorithm.
+    Searches for 'key' in sorted array 'arr'.
+    Returns the index of 'key' if found, else -1.
+    """
+    l = 0
+    r = len(arr) - 1
+    while l <= r:
+        m = (l + r) // 2
+        if arr[m] == key:
+            return m
+        elif arr[m] > key:
+            r = m - 1
+        else:
+            l = m + 1
+    return -1
+
+def linear_search(arr, key):
+    """ Implements the linear search algorithm.
+    Searches for 'key' in array 'arr'.
+    Returns the index of 'key' if found, else -1.
+    """             
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
